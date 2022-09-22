c2f2fcf82879e5187f98b09bc01fe3119c47096e
Bassem Hassan <bassem.hassan1995@gmail.com>
2022-09-22 16:29:51 +0200

    Initial commit

---
+++ app/src/main/java/bassem/ahoy/weather/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package bassem.ahoy.weather.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import bassem.ahoy.weather.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
